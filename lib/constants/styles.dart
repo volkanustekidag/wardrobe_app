a6eb36abc475ef17adadf6d124456f6e8495e1cf
volkanustekidag <volkanustekidg@gmail.com>
2022-06-29 16:31:09 +0300

    styles file created

---
+++ lib/constants/styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const TextStyle titleStyleBold =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 36);
+
+const TextStyle titleStyleRegular =
+    TextStyle(fontWeight: FontWeight.w400, fontSize: 20);
+
+List<BoxShadow> textFromFieldShadow = [
+  BoxShadow(
+      color: Color(0xFFF6F6F6),
+      blurRadius: 5,
+      spreadRadius: 0,
+      offset: Offset.fromDirection(-4, 4))
+];
