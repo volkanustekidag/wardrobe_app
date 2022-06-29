1d8fb9cdea36eb6b1af66ed2e5481349695d79c6
volkanustekidag <volkanustekidg@gmail.com>
2022-06-29 16:30:32 +0300

    text field form widget created

---
+++ lib/components/textfieldform.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:wardrobe_app/constants/styles.dart';
+
+Padding textFromFieldBuild(String hintText) {
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 20),
+    child: Container(
+      decoration: BoxDecoration(
+          boxShadow: textFromFieldShadow,
+          color: Color(0xFFF6F6F6),
+          borderRadius: BorderRadius.circular(10)),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: hintText.contains("E-mail") ? false : true,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: hintText,
+            prefixText: "   ",
+            hintStyle: TextStyle(color: Color(0xff969696))),
+      ),
+    ),
+  );
+}
