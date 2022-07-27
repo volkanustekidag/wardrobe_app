0d7927b2bb1f665b392e2379693696a81ba075e3
volkanustekidag <volkanustekidg@gmail.com>
2022-07-27 22:10:59 +0300

    Added new strings to the String class

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -37,3 +37,6 @@ const svgCategoryList = [
   woman_bag,
   woman_jacket,
 ];
+
+const photoUpdatedDescriptionText =
+    "*In order for your clothes to look good in the gallery, make sure that the background is a single color.";
