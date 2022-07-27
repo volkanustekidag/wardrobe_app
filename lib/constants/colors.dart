fcee012694d20cc87e4824fa6682e155fecfe608
volkanustekidag <volkanustekidg@gmail.com>
2022-07-27 22:09:07 +0300

    Added new colors to color class

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xff323232);
 const Color secColor = Color(0xffF6F6F6);
 const Color textColor = Color(0xff969696);
+const iconColor = Color(0XFF3F3F3F);
+const seasonButtonColor = Color(0xffD9D9D9);
