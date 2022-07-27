be012890f33089714f6d5e2521f2f7ae56738020
volkanustekidag <volkanustekidg@gmail.com>
2022-07-27 22:10:07 +0300

    Added new Box Decorations const

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wardrobe_app/constants/colors.dart';
 
 double deviceHeight(context) => MediaQuery.of(context).size.height;
 double deviceWidth(context) => MediaQuery.of(context).size.width;
+
+const BoxDecoration modalBottomSheetBoxDecoration = BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.only(
+      topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+);
+
+const BoxDecoration boxDecorationItem = BoxDecoration(
+    color: secColor,
+    borderRadius: BorderRadius.only(
+        topRight: Radius.circular(30),
+        topLeft: Radius.circular(30),
+        bottomLeft: Radius.circular(30),
+        bottomRight: Radius.circular(30)));
