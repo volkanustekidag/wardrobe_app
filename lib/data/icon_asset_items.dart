e015ae07d04755a11ff7e50f5ee47f41b0ca99c8
volkanustekidag <volkanustekidg@gmail.com>
2022-08-02 17:25:05 +0300

    Category icons data class created.

---
+++ lib/data/icon_asset_items.dart
@@ -0,0 +1,41 @@
+class IconAssetItems {
+  static const svgCategoryList = [
+    bikini,
+    bra,
+    dress,
+    girl_tshirt,
+    high_heels,
+    hoodie,
+    jean,
+    jewelry,
+    man_bag,
+    man_hat,
+    man_shoes,
+    skirt,
+    skirt2,
+    sunglasses,
+    tie,
+    watch,
+    woman_bag,
+    woman_jacket,
+  ];
+
+  static const String bikini = "assets/svg/bikini.svg";
+  static const String bra = "assets/svg/bra.svg";
+  static const String dress = "assets/svg/dress.svg";
+  static const String girl_tshirt = "assets/svg/girl_tshirt.svg";
+  static const String high_heels = "assets/svg/high_heels.svg";
+  static const String hoodie = "assets/svg/hoodie.svg";
+  static const String jean = "assets/svg/jean.svg";
+  static const String jewelry = "assets/svg/jewelry.svg";
+  static const String man_bag = "assets/svg/man_bag.svg";
+  static const String man_hat = "assets/svg/man_hat.svg";
+  static const String man_shoes = "assets/svg/man_shoes.svg";
+  static const String skirt = "assets/svg/skirt.svg";
+  static const String skirt2 = "assets/svg/skirt2.svg";
+  static const String sunglasses = "assets/svg/sunglasses.svg";
+  static const String tie = "assets/svg/tie.svg";
+  static const String watch = "assets/svg/watch.svg";
+  static const String woman_bag = "assets/svg/woman_bag.svg";
+  static const String woman_jacket = "assets/svg/woman_jacket.svg";
+}
