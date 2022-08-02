88ae56a67bf9379f36f4a890cff1088da4bc61f2
volkanustekidag <volkanustekidg@gmail.com>
2022-08-02 13:56:00 +0300

    Added new colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,4 +4,5 @@ const Color primaryColor = Color(0xff323232);
 const Color secColor = Color(0xffF6F6F6);
 const Color textColor = Color(0xff969696);
 const iconColor = Color(0XFF3F3F3F);
-const seasonButtonColor = Color(0xffD9D9D9);
+const seasonButtonHintColor = Color(0xffD9D9D9);
+const favoriteIconColor = Color(0xffABABAB);
