6aa9d5543133e77edc1adbda2c3dc45cddb579cf
volkanustekidag <volkanustekidg@gmail.com>
2022-08-02 17:19:46 +0300

    Menu Item model created

---
+++ lib/models/menu_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final IconData iconData;
+
+  const MenuItem({@required this.iconData, @required this.title});
+}
