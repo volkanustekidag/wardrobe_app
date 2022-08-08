64cc565a552961806ed7375e4f9f5921beb4c81e
volkanustekidag <volkanustekidg@gmail.com>
2022-08-08 11:39:04 +0300

    Secondary color value changed

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xff323232);
-const Color secColor = Color(0xffF6F6F6);
+const Color secColor = Color(0xffECECEE);
 const Color textColor = Color(0xff969696);
 const iconColor = Color(0XFF3F3F3F);
 const seasonButtonHintColor = Color(0xffD9D9D9);
