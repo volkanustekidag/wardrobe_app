bcb71482bc09d64797ce5261fe8c8f8021afe48d
volkanustekidag <volkanustekidg@gmail.com>
2022-08-08 12:00:24 +0300

    Block has been added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wardrobe_app/blocs/bloc_observer.dart';
+import 'package:wardrobe_app/blocs/bottom_nav_bloc/bottom_nav_bloc.dart';
 import 'package:wardrobe_app/constants/colors.dart';
 import 'package:wardrobe_app/pages/home_page/home_page.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(MyApp());
+  BlocOverrides.runZoned(() => runApp(MyApp()),
+      blocObserver: AppBlocObserver());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Sizer(builder: (context, orientation, deviceType) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            accentColor: primaryColor,
-            bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              backgroundColor: primaryColor,
-            ),
-            appBarTheme: AppBarTheme(
-              backgroundColor: primaryColor,
-            )),
-        home: HomePage(),
-      );
-    });
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => BottomNavBloc(),
+        )
+      ],
+      child: Sizer(builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              accentColor: primaryColor,
+              bottomNavigationBarTheme: BottomNavigationBarThemeData(
+                backgroundColor: primaryColor,
+              ),
+              appBarTheme: AppBarTheme(
+                backgroundColor: primaryColor,
+              )),
+          home: HomePage(),
+        );
+      }),
+    );
   }
 }
