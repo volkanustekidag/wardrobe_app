6ddacd6b80d7e4082bfc349ecb608052f5a26159
volkanustekidag <volkanustekidg@gmail.com>
2022-08-09 12:27:43 +0300

    Added app bar and remove welcome container

--- lib/pages/category_page/category_page.dart
+++ lib/pages/category_page/category_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wardrobe_app/components/textfieldform.dart';
 import 'package:wardrobe_app/constants/colors.dart';
 import 'package:wardrobe_app/constants/const.dart';
@@ -20,7 +21,6 @@ class _CategoryPageState extends State<CategoryPage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _generateCatogeryIconList();
   }
@@ -28,42 +28,19 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Virtual Wardrobe",
+          style: GoogleFonts.bebasNeue(color: primaryColor, fontSize: 20.sp),
+        ),
+        centerTitle: true,
+      ),
       body: Container(
         child: Column(
           children: [
             Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: primaryColor,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Welcome Alisya!",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text("Welcome to your Virtual Locker!",
-                            style: TextStyle(color: Colors.white, fontSize: 13))
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-                flex: 5,
+                flex: 7,
                 child: GridView.count(
                     crossAxisCount: 3,
                     children: List.generate(10, (index) {
@@ -113,12 +90,10 @@ class _CategoryPageState extends State<CategoryPage> {
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Container(
-                              width: 40,
-                              height: 40,
                               decoration: BoxDecoration(
-                                  color: Colors.transparent,
+                                  color: Colors.white,
                                   border:
-                                      Border.all(color: primaryColor, width: 2),
+                                      Border.all(color: primaryColor, width: 1),
                                   borderRadius: BorderRadius.circular(20)),
                               child: Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
