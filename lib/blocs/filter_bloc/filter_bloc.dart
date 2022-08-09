2bc537dbf7f60dd70c21df35c4699066f29209cb
volkanustekidag <volkanustekidg@gmail.com>
2022-08-09 23:54:23 +0300

    Created filter bloc

---
+++ lib/blocs/filter_bloc/filter_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'filter_event.dart';
+part 'filter_state.dart';
+
+class FilterBloc extends Bloc<FilterEvent, FilterState> {
+  FilterBloc() : super(FilterInitial()) {
+    on<ChangeFilterVisibilityEvent>((event, emit) {
+      emit(FilterVisibilityState(visible: event.visibility));
+    });
+  }
+}
