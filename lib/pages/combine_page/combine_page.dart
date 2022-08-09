00f4efdb256e46340f11471ab4cdf5444b32bebe
volkanustekidag <volkanustekidg@gmail.com>
2022-08-09 12:48:52 +0300

    Added app bar and animated opacity for textfrom

--- lib/pages/combine_page/combine_page.dart
+++ lib/pages/combine_page/combine_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:wardrobe_app/constants/colors.dart';
 import 'package:wardrobe_app/constants/const.dart';
 import 'package:sizer/sizer.dart';
-import 'package:wardrobe_app/pages/combine_detail_page/combine_detail.dart';
-import 'package:wardrobe_app/pages/make_combine_page.dart/make_combine_pages.dart';
+import 'package:wardrobe_app/constants/styles.dart';
+import 'package:wardrobe_app/pages/combine_detail_page/combine_detail_page.dart';
+import 'package:wardrobe_app/pages/make_combine_page/make_combine_pages.dart';
 
 class CombinePage extends StatefulWidget {
   const CombinePage({Key key}) : super(key: key);
@@ -20,6 +23,28 @@ class _CombinePageState extends State<CombinePage> {
     "assets/dress3.png",
   ];
 
+  final ScrollController scrollcontroller = new ScrollController();
+  bool scrollVisibility = true;
+
+  @override
+  void initState() {
+    scrollcontroller.addListener(() {
+      if (scrollcontroller.position.pixels > 0) {
+        setState(() {
+          scrollVisibility = false;
+        });
+        print(scrollcontroller.position.pixels);
+        print(scrollcontroller.position.maxScrollExtent);
+      } else {
+        print(scrollVisibility);
+        setState(() {
+          scrollVisibility = true;
+        });
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,46 +56,66 @@ class _CombinePageState extends State<CombinePage> {
           label: Row(
             children: [Icon(Icons.add), Text("Add Combine")],
           )),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Combines",
+          style: GoogleFonts.bebasNeue(color: primaryColor, fontSize: 20.sp),
+        ),
+      ),
       body: Column(
         children: [
-          Container(
-              height: deviceHeight(context) * 0.08,
-              decoration: BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10))),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
+          AnimatedOpacity(
+            duration: Duration(seconds: 1),
+            opacity: scrollVisibility ? 1 : 0,
+            child: Visibility(
+              visible: scrollVisibility,
+              child: Container(
+                  height: deviceHeight(context) * 0.08,
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)),
-                  height: deviceHeight(context) * 0.07,
-                  child: TextFormField(
-                      onChanged: (text) {},
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Color(0xffD0D0D2),
-                        ),
-                        hintText: "Search Combine",
-                        hintStyle: TextStyle(
-                            color: Colors.black12, fontWeight: FontWeight.w400),
-                        border: InputBorder.none,
-                      )),
-                ),
-              )),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: textFromFieldShadow,
+                          borderRadius: BorderRadius.circular(10)),
+                      height: deviceHeight(context) * 0.07,
+                      child: TextFormField(
+                          onChanged: (text) {},
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: primaryColor,
+                            ),
+                            hintText: "Search Combine",
+                            hintStyle: TextStyle(
+                                color: primaryColor,
+                                fontWeight: FontWeight.w400),
+                            border: InputBorder.none,
+                          )),
+                    ),
+                  )),
+            ),
+          ),
           Flexible(
             child: ListView.builder(
+              controller: scrollcontroller,
               itemCount: 5,
               itemBuilder: (context, index) {
                 return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Container(
-                    height: deviceHeight(context) * 0.25,
-                    color: secColor,
+                    height: deviceHeight(context) * 0.2,
+                    decoration: BoxDecoration(
+                      boxShadow: textFromFieldShadow,
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                     child: GestureDetector(
                       onTap: () {
                         Navigator.push(
@@ -78,36 +123,33 @@ class _CombinePageState extends State<CombinePage> {
                             MaterialPageRoute(
                                 builder: (context) => CombineDetailPage()));
                       },
-                      child: Hero(
-                        tag: index,
-                        child: Column(
-                          children: [
-                            Flexible(
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: 4,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: EdgeInsets.all(20),
-                                    child: Image.asset(
-                                      pathList[index],
-                                      width: 25.w,
-                                    ),
-                                  );
-                                },
-                              ),
+                      child: Column(
+                        children: [
+                          Flexible(
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 4,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: EdgeInsets.all(20),
+                                  child: Image.asset(
+                                    pathList[index],
+                                    width: 25.w,
+                                  ),
+                                );
+                              },
                             ),
-                            DefaultTextStyle(
-                              style: TextStyle(),
-                              child: Text(
-                                "My Summer Collection",
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            )
-                          ],
-                        ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "My Summer Collection",
+                              style: TextStyle(
+                                  color: primaryColor,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
                       ),
                     ),
                   ),
