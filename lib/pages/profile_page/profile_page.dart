da184672a5425769a9d054bb028fedcbd644d8ff
volkanustekidag <volkanustekidg@gmail.com>
2022-08-12 21:10:27 +0300

    Updated profile page

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wardrobe_app/constants/colors.dart';
 import 'package:sizer/sizer.dart';
 import 'package:wardrobe_app/constants/const.dart';
-import 'package:wardrobe_app/constants/styles.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key key}) : super(key: key);
@@ -20,63 +22,172 @@ class _ProfilePageState extends State<ProfilePage> {
         elevation: 0,
         backgroundColor: Colors.white,
         title: Text(
-          "Fernanda Morales",
+          "Tuba Günaçgün",
           style: GoogleFonts.bebasNeue(color: primaryColor, fontSize: 20.sp),
         ),
-        actions: [
-          Icon(
-            Icons.exit_to_app_rounded,
-            color: primaryColor,
-          )
-        ],
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: textFromFieldShadow,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30))),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "Clothes",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 14.sp),
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: linearGradientList[
+                        Random().nextInt(linearGradientList.length)])),
+            height: deviceHeight(context),
+          ),
+          Column(
+            children: [
+              Container(
+                height: deviceHeight(context) * 0.15,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30))),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset(
+                                  "assets/svg/price_tag.svg",
+                                  width: 10.w,
+                                ),
+                                Text(
+                                  "Price",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12.sp),
+                                ),
+                                Text("400\$")
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset(
+                                  "assets/svg/hanger.svg",
+                                  width: 10.w,
+                                ),
+                                Text(
+                                  "Clothes",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12.sp),
+                                ),
+                                Text("10")
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SvgPicture.asset(
+                                  "assets/svg/bComb.svg",
+                                  width: 10.w,
+                                ),
+                                Text(
+                                  "Combines",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 12.sp),
+                                ),
+                                Text("10")
+                              ],
+                            ),
+                          ],
                         ),
-                        Text("10")
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "Combines",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 14.sp),
-                        ),
-                        Text("10")
-                      ],
-                    ),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              SizedBox(
+                height: deviceHeight(context) * 0.15,
+              ),
+              Flexible(
+                child: Container(
+                  height: deviceHeight(context) * 0.6,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      )),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Container(
+                      child: ListView(
+                        children: [
+                          ProfileCardItem(
+                            title: "Update Password",
+                            iconData: Icons.lock_open_sharp,
+                          ),
+                          ProfileCardItem(
+                            title: "E-mail Verifyd",
+                            iconData: Icons.mark_email_unread_outlined,
+                          ),
+                          ProfileCardItem(
+                            title: "Delete Account",
+                            iconData: Icons.delete_outline,
+                          ),
+                          ProfileCardItem(
+                            title: "App Settings",
+                            iconData: Icons.app_settings_alt_outlined,
+                          ),
+                          ProfileCardItem(
+                            title: "Exit",
+                            iconData: Icons.exit_to_app_outlined,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: deviceHeight(context) * 0.7,
-            ),
-          )
         ],
+      ),
+    );
+  }
+}
+
+class ProfileCardItem extends StatelessWidget {
+  final title;
+  final iconData;
+  const ProfileCardItem({
+    Key key,
+    this.title,
+    this.iconData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: ("Exit" == title || "Update Password" == title) ? 0 : 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+        margin: EdgeInsets.zero,
+        child: ListTile(
+          leading: Icon(
+            iconData,
+            color: primaryColor,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 12.sp,
+                color: primaryColor),
+          ),
+        ),
       ),
     );
   }
