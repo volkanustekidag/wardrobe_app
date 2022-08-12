66a9fed74b5278222e600c009255a2a2ddc36b9e
volkanustekidag <volkanustekidg@gmail.com>
2022-08-12 21:03:21 +0300

    Added linear gradient color and list

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -6,3 +6,42 @@ const Color textColor = Color(0xff969696);
 const iconColor = Color(0XFF3F3F3F);
 const seasonButtonHintColor = Color(0xffD9D9D9);
 const favoriteIconColor = Color(0xffABABAB);
+
+//linear gradient color lists
+const List<Color> summerDog = [Color(0xffa8ff78), Color(0xff78ffd6)];
+const List<Color> piggyPing = [Color(0xffee9ca7), Color(0xffffdde1)];
+const List<Color> argon = [
+  Color(0xff03001e),
+  Color(0xff7303c0),
+  Color(0xffec38bc),
+  Color(0xfffdeff9)
+];
+
+const List<Color> jShine = [
+  Color(0xff12c2e9),
+  Color(0xffc471ed),
+  Color(0xfff64f59)
+];
+
+const List<Color> wiretap = [
+  Color(0xff8a2387),
+  Color(0xffe94957),
+  Color(0xfff27121)
+];
+
+const List<Color> flare = [Color(0xfff12711), Color(0xfff5af19)];
+const List<Color> purpink = [Color(0xff7f00ff), Color(0xffe100ff)];
+const List<Color> sublimeLight = [Color(0xfffc5c7d), Color(0xff6a82fb)];
+const List<Color> seaWeed = [Color(0xff4cb8c4), Color(0xff3cd3ad)];
+
+const linearGradientList = [
+  summerDog,
+  argon,
+  piggyPing,
+  sublimeLight,
+  jShine,
+  flare,
+  seaWeed,
+  purpink,
+  wiretap
+];
